a5254f3f5db93cf510caec4a77866f59e64408d1
valeriana <93216658+jaguarwithathirdeye@users.noreply.github.com>
2022-01-31 16:14:37 +0300

    Add files via upload

---
+++ lesson3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    float b = sqrt((float)a);
+    for (int i = 2; i <= b; ++i)
+    {
+        if (a % i == 0) {
+            cout << "ne prostoe" << endl;
+            return 0;
+        }
+    }
+    cout << "prostoe" << endl;
+
+    return 0;
+}
